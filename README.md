d8c5a4b7773fc2c6e720a5714d78a559c7d4eac6
Mirko Lenz <mirko@mirkolenz.com>
2022-12-17 22:53:41 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# CI Preset for Semantic Release with NPM
+
+A shareable semantic-release configuration for NPM packages.
+It is inspired by [bjoluc's config](https://github.com/bjoluc/semantic-release-config-npm).
---
+++ release.config.js
@@ -0,0 +1,46 @@
+const config = {
+  branches: [
+    { name: "main" },
+    { name: "next" },
+    { name: "+([0-9])?(.{+([0-9]),x}).x" },
+    { name: "dev", prerelease: true },
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogTitle: "# Changelog",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        tarballDir: "dist",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: "dist/*.tgz",
+        // failComment: false,
+      },
+    ],
+    "@semantic-release/git",
+  ],
+};
+
+module.exports = config;
